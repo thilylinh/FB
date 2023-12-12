f13d48d2e2483b172c7890627f7db7ff25abdf4e
Linh Thi <linh.ly@bitvillegaming.com>
2023-12-12 20:09:43 +0700

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,47 +1,16 @@
 /* eslint-disable */
 import { createRouter, createWebHistory } from "vue-router";
-import { getStateVueX, isStrEmpty } from "@/utils/utils";
-
-import publicRoutes from "@/router/public-routes";
-// import authenticateRoutes from "./authenticate-routes";
-import authenticatedRoutes from "./authenticated-routes";
+import publicRoutes from "@/router/public-routes"
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes: [
-        ...publicRoutes,
-        //...authenticateRoutes,
-        ...authenticatedRoutes,
-        {
-            path: "/*",
-            name: "NotFound",
-            component: () =>
-                import(
-                    "@/views/404/error404.vue"
-                ),
-        },
+        ...publicRoutes
     ],
 });
 
 router.beforeEach((to, from, next) => {
-    const langSearchParams: any = to.query?.lang || null;
-    const stateStore = getStateVueX();
-
-    const token = stateStore.token;
-    if (langSearchParams) {
-        localStorage.setItem("locale", langSearchParams);
-    }
-
-    const hasMeta = (name: string): boolean =>
-        to.matched.some((record) => record.meta[name]);
-
-    if (hasMeta("authenticated") && isStrEmpty(token)) {
-        next({ name: "Login", query: to.query });
-    } else if (hasMeta("guest") && !isStrEmpty(token)) {
-        next({ name: "Home Page", query: to.query });
-    } else {
-        next();
-    }
+    next();
 });
 
 export default router;
